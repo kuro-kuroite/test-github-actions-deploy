e5a40fcaa4f63684f72922926f19d00fe1c15e0d
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2021-01-23 22:37:05 +0900

    chore: test renovate

--- renovate-config.js
+++ renovate-config.js
@@ -2,8 +2,9 @@ module.exports = {
 	branchPrefix: "renovate/",
 	dryRun: true,
 	gitAuthor: "Renovate Bot <bot@renovateapp.com>",
-	onboarding: false,
+	onboarding: true,
+	onboardingBranch: "renovate/configure",
 	platform: "github",
 	includeForks: true,
-	repositories: ["kuro-kuroite/test-github-actions-deploy"],
+	repositories: [],
 };
