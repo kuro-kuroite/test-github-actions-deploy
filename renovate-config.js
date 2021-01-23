a76deac29795ac96199387564b7bd18ce340ffc9
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2021-01-23 21:59:40 +0900

    chore: test renovate

--- renovate-config.js
+++ renovate-config.js
@@ -1,27 +1,9 @@
 module.exports = {
-  branchPrefix: 'test-renovate/',
-  dryRun: true,
-  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
-  onboarding: false,
-  platform: 'github',
-  includeForks: true,
-  repositories: [
-    'renovatebot/github-action',
-    'renovate-tests/cocoapods1',
-    'renovate-tests/gomod1',
-  ],
-  packageRules: [
-    {
-      description: 'lockFileMaintenance',
-      updateTypes: [
-        'pin',
-        'digest',
-        'patch',
-        'minor',
-        'major',
-        'lockFileMaintenance',
-      ],
-      stabilityDays: 0,
-    },
-  ],
+	branchPrefix: "renovate/",
+	dryRun: true,
+	gitAuthor: "Renovate Bot <bot@renovateapp.com>",
+	onboarding: false,
+	platform: "github",
+	includeForks: true,
+	repositories: ["kuro-kuroite/test-github-actions-deploy"],
 };
